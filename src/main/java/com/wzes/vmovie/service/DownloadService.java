2758fbb6969f512b042a75b68b5598bc79d94a58
wzes <793890838@qq.com>
2017-11-07 23:02:55 +0800

    update downloadservice

--- src/main/java/com/wzes/vmovie/service/DownloadService.java
+++ src/main/java/com/wzes/vmovie/service/DownloadService.java
@@ -36,7 +36,6 @@ public class DownloadService {
      */
     public List<MovieLink> findLink(String movieName) throws IOException {
 
-
         List<MovieLink> movieLinks = new ArrayList<>();
         OkHttpClient okHttpClient = new OkHttpClient();
         FormBody.Builder formBody = new FormBody.Builder();
@@ -104,17 +103,4 @@ public class DownloadService {
 
         return movieLinks;
     }
-
-
-    //        Headers headers = new Headers.Builder()
-//                .add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3192.0 Safari/537.36")
-//                .add("Content-Type", "application/xml")
-//                .add("Referer", "http://www.6vhao.tv/")
-//                .add("Cookie", "UM_distinctid=15e5c9b3a8e390-0f3f3073bdbd1a-632a7124-1fa400-15e5c9b3a8fe41; ecmslastsearchtime=1508979742; CNZZDATA1260799993=812339623-1504790582-null%7C1508975099")
-//                .add("Host", "www.6vhao.tv")
-//                .add("Connection", "keep-alive")
-//                .add("Accept-Encoding", "gzip, deflate")
-//                .add("Accept-Language", "en-US,zh-CN;q=0.8,zh;q=0.6,en;q=0.4")
-//                .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-//                .build();
 }
